c88e8c7941c0c52929c2c3aa8a04ca3a267fc76e
wannacry3030 <wannacry.bat@proton.me>
2023-04-24 17:03:50 -0300

    update script.js

--- script.js
+++ script.js
@@ -13,17 +13,49 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class Workout {
   date = new Date();
-  id = 
+  id = (new Date() + '').slice(-10);
 
   constructor(coords, distance, duration) {
-    this.coords = coords;
+    this.coords = coords; // [lat,lng]
     this.distance = distance; // in KM
     this.duration = duration; // in Min
   }
 }
 
-let map, mapEvent;
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
 
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 92, 523);
+console.log(run1, cycling1);
+
+//////////////////////////////////////////////////////
+//APP ARCHITECTURE
 class App {
   #map;
   #mapEvent;
