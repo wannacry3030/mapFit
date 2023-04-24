82880c206e21cca91a9b26398364c88819dd8c59
wannacry3030 <wannacry.bat@proton.me>
2023-04-24 14:38:39 -0300

    update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ if (navigator.geolocation)
 
       const map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
