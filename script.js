d232adf68fc79bae778b4f0f65ecf546c2b3b4b7
wannacry3030 <wannacry.bat@proton.me>
2023-04-24 22:36:34 -0300

    update script.js

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -109,27 +110,48 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
 
     //get data from form
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
-
-    //check if data is valid
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
     //if workout running, create running object
     if (type === 'running') {
+      //check if state is valid
       const cadence = +inputCadence.value;
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      const workout = new Running([lat, lng], distance, duration, cadence);
     }
+
     //if workout cycling, create cycling object
     if (type === 'cycling') {
       const elevation = +inputCadence.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
     }
     //add new object to workout array
+    this.#workouts.push(workout);
 
     //render workout on map ass marker
-    const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
